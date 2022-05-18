a038de4f6f1cfb21bd73ffd0a4a8b70d42c84e2d
ssdj0607 <ssdj0607@naver.com>
2022-05-18 22:45:58 +0900

    롬복 사용하여 dto 추가 Hello Controller 파라미터 추가

--- src/main/java/com/example/book/web/HelloController.java
+++ src/main/java/com/example/book/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.book.web;
 
+import com.example.book.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 
 }
