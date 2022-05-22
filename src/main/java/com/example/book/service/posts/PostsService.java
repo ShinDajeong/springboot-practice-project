5bda41c629b93397e50cb73eccf8fe7fce2adb6d
ssdj0607 <ssdj0607@naver.com>
2022-05-22 16:52:34 +0900

    JPA 적용 및 등록 API 구현

---
+++ src/main/java/com/example/book/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.example.book.service.posts;
+
+import com.example.book.domain.posts.PostsRepository;
+import com.example.book.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ src/main/java/com/example/book/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,29 @@
+package com.example.book.web.dto;
+
+import com.example.book.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
